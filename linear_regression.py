2e68333c64dbce3c587fd032bdab9a9ae90d6f2e
theDrake1010 <40403909+theDrake1010@users.noreply.github.com>
2019-09-16 23:06:01 +0530

    added simple accuracy

--- linear_regression.py
+++ linear_regression.py
@@ -19,7 +19,6 @@ def load_data(loc):
     print(data.dtype)
     return data, data.shape
 
-
 # SEPARATING TEST AND TRAINING DATA...
 def split_data(data):
     train_data = data[:int(data.shape[0] * 0.8), :]
@@ -28,7 +27,6 @@ def split_data(data):
     test_size = test_data.shape
     print(train_size, test_size)
     return train_data, test_data, train_size, test_size
-
 
 # CREATING FEATURE MATRICES AND TARGET VECTORS...
 def create_features(train_data, test_data):
@@ -39,9 +37,7 @@ def create_features(train_data, test_data):
     print("\n\n")
     return train_features, train_targets, test_features, test_targets
 
-
 # NORMALIZE FEATURES...
-
 def normalize_features(train_features, test_features):
     print("Features have been normalized, the max and min Z-scores are: \n")
     print("Training data: ")
@@ -62,10 +58,7 @@ def normalize_features(train_features, test_features):
     print("\n\n")
     return train_features, test_features
 
-
 # ADDING x0 FEATURES:
-
-
 def add_bias(train_features, test_features):
     train_size = train_data.shape
     test_size = test_data.shape
@@ -75,17 +68,41 @@ def add_bias(train_features, test_features):
     test_features = np.hstack([tx0, test_features])
     return train_features, test_features
 
-
 # Defining cost function...
-
 def cost(features, targets, params):
     m = targets.shape[0]
     j = np.sum((features.dot(params) - targets) ** 2) / (2 * m)
     return j
 
+# Error analysis and comparision to sklearn...
+def root_mean_sq_err(targets, pred):
+    rmse = np.sqrt(sum((targets - pred) ** 2) / targets.shape[0])
+    return rmse
+
+def r2_score(Y, Y_pred):
+    mean_y = np.mean(Y)
+    ss_tot = sum((Y - mean_y) ** 2)
+    ss_res = sum((Y - Y_pred) ** 2)
+    r2 = 1 - (ss_res / ss_tot)
+    return r2
+
+def simpleAccuracy(Y,Y_pred,epsilon):
+    hits=0
+    miss=0
+
+    for i in range(len(Y)):
+        #print(Y[i],Y_pred[i])
+        if Y[i] - Y_pred[i] <= epsilon:
+            hits += 1
+        else:
+            miss += 1
+
+    acc = hits/(hits+miss)
+    return acc
+
+    
 
 # Defining gradient descent algorithm...
-
 def grad_des(features, targets, params, alpha, maxiter):
     history = [0] * maxiter
     m = targets.shape[0]
@@ -98,75 +115,60 @@ def grad_des(features, targets, params, alpha, maxiter):
     return params, history
 
 
+
 # Lets get this started...
+if __name__ == "__main__":
 
-data, data_size = load_data("mpg.csv")
-train_data, test_data, train_size, test_size = split_data(data)
-train_features, train_targets, test_features, test_targets = create_features(train_data, test_data)
-train_features, test_features = normalize_features(train_features, test_features)
-train_features, test_features = add_bias(train_features,test_features)
+    data, data_size = load_data("mpg.csv")
+    train_data, test_data, train_size, test_size = split_data(data)
+    train_features, train_targets, test_features, test_targets = create_features(train_data, test_data)
+    train_features, test_features = normalize_features(train_features, test_features)
+    train_features, test_features = add_bias(train_features,test_features)
 
-# Defining initial parameters...
+    # Defining initial parameters...
 
-ini_params = np.array([0] * train_features.shape[1])
-alpha = 0.03
-max_iter = 800
+    ini_params = np.array([0] * train_features.shape[1])
+    alpha = 0.03
+    max_iter = 800
 
-# Calling Gradient descent algorithm...
+    # Calling Gradient descent algorithm...
 
-new_params, cost_history = grad_des(train_features, train_targets, ini_params, alpha, max_iter)
+    new_params, cost_history = grad_des(train_features, train_targets, ini_params, alpha, max_iter)
 
-# Plots to help visualize effectiveness of learning rate...
+    # Plots to help visualize effectiveness of learning rate...
 
-plt.plot(range(1, max_iter + 1), cost_history)
-plt.xlabel("ITERATIONS")
-plt.ylabel("COST")
-plt.title("Cost V/S Iterations")
-plt.show()
-
-
-print("The optimum parameters for the given data set is: \n", new_params)
+    plt.plot(range(1, max_iter + 1), cost_history)
+    plt.xlabel("ITERATIONS")
+    plt.ylabel("COST")
+    plt.title("Cost V/S Iterations")
+    plt.show()
 
 
-
-# Error analysis and comparision to sklearn...
-
-def root_mean_sq_err(targets, pred):
-    rmse = np.sqrt(sum((targets - pred) ** 2) / targets.shape[0])
-    return rmse
-
-
-def r2_score(Y, Y_pred):
-    mean_y = np.mean(Y)
-    ss_tot = sum((Y - mean_y) ** 2)
-    ss_res = sum((Y - Y_pred) ** 2)
-    r2 = 1 - (ss_res / ss_tot)
-    return r2
-
-
+    print("The optimum parameters for the given data set is: \n", new_params)
 # Printing Error values of my model...
 
-print("\n\nError values: (Lower is better) : \nMY PREDICTOR:")
-predictions = test_features.dot(new_params)
-print("The root mean square error is: ", root_mean_sq_err(test_targets, predictions))
-print("The r^2 score is: ", r2_score(test_targets, predictions), "\n\n")
+    print("\n\nError values: (Lower is better) : \nMY PREDICTOR:")
+    predictions = test_features.dot(new_params)
+    print("The root mean square error is: ", root_mean_sq_err(test_targets, predictions))
+    print("The r^2 score is: ", r2_score(test_targets, predictions), "\n\n")
 
 
-# Building a linear regressor from Sklearn for comparision...
+    # Building a linear regressor from Sklearn for comparision...
 
-reg = lin()
-reg.fit(train_features, train_targets)
-skpred = reg.predict(test_features)
+    reg = lin()
+    reg.fit(train_features, train_targets)
+    skpred = reg.predict(test_features)
 
-# Printing Sklearn's error values...
+    # Printing Sklearn's error values...
 
-print("SKLEARN :")
-print("The root mean square error of sklearn is: ", root_mean_sq_err(test_targets, skpred))
-print("The r^2 score of sklearn is: ", r2_score(test_targets, skpred), "\n\n")
+    print("SKLEARN :")
+    print("The root mean square error of sklearn is: ", root_mean_sq_err(test_targets, skpred))
+    print("The r^2 score of sklearn is: ", r2_score(test_targets, skpred), "\n\n")
 
+    errRate=0.3
+    print(f"The accuracy for the model is {simpleAccuracy(test_targets,predictions,errRate)}")
 
-# Printing first 10 predictions of my model and sklearn side by side to actual values...
-
-print("ACTUAL \t\t\t\t MY MODEL \t\t\t\t\t SKLEARN\n")
-for i in range(10):
-    print(test_targets[i], "\t", predictions[i], "\t", skpred[i])
+    # Printing first 10 predictions of my model and sklearn side by side to actual values...
+    #print("ACTUAL \t\t\t\t MY MODEL \t\t\t\t\t SKLEARN\n")
+    #for i in range(10):
+    #    print(test_targets[i], "\t", predictions[i], "\t", skpred[i])
